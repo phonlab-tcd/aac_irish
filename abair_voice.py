57276cb455f798e9d323c7aade7356c6aa8f24c4
Oisin Thomas Morrin <morrino@tcd.ie>
2022-01-22 13:09:25 +0000

    List ABAIR voices

---
+++ abair_voice.py
@@ -0,0 +1,3 @@
+import requests
+
+voices = {"irish_1":"ga_UL_anb_exthts", "irish_2":"ga_UL_anb_nnmnkwii", "irish_3":"ga_CO_pmc_exthts", "irish_4":"ga_CO_pmg_nnmnkwii", "irish_5":"ga_CO_snc_exthts", "irish_6":"ga_CO_snc_nnmnkwii", "irish_7":"ga_MU_nnc_exthts", "irish_8":"ga_MU_nnc_nnmnkwii", "irish_9":"ga_MU_cmg_nnmnkwii"}
