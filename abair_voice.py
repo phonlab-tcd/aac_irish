5fa3f8e262d0c98f0ecb5a362aaa0e73388e9a24
Harald <harald@vps-00434643.vps.ovh.net>
2022-07-19 08:02:36 +0000

    changed default voice to nemo snc

--- abair_voice.py
+++ abair_voice.py
@@ -1,5 +1,12 @@
 import requests
 
+
+from joblib import Memory
+cachelocation = 'synthesis_cache'
+memory = Memory(location=cachelocation, bytes_limit=100000)
+
+
+@memory.cache    
 def get_voice(text,voice_type):
     voices = {"Ulster":"ga_UL_anb_exthts", "Connaught":"ga_CO_pmc_exthts", "Connaught (boy)":"ga_CO_snc_exthts", "Connaught (girl)":"ga_CO_snc_exthts", "Munster":"ga_MU_nnc_exthts"}
     receive = None
@@ -7,6 +14,9 @@ def get_voice(text,voice_type):
         receive = requests.get(f"https://abair.ie/api2/synthesise?input={text}&voice={voices[voice_type]}&audioEncoding=MP3&timing=WORD&htsParams=-fm%206.9%20-a%200.43%20-r%200.8")
     elif voice_type == "Connaught (girl)":
         receive = requests.get(f"https://abair.ie/api2/synthesise?input={text}&voice={voices[voice_type]}&audioEncoding=MP3&timing=WORD&htsParams=-fm%203%20-a%200.45%20-r%200.8")
+    elif voice_type == "Ulster":
+        receive = requests.get(f"https://phoneticsrv3.lcs.tcd.ie/nemo/synthesise?outputType=JSON&audioEncoding=mp3&voice=snc.multidialect&input={text}")
+
     else:
         receive = requests.get(f"https://abair.ie/api2/synthesise?input={text}&voice={voices[voice_type]}&audioEncoding=MP3&timing=WORD")
     return receive
